1743db16e483af863c3c01b69dcdf4bc2f7408e4
Lawangin Khan <lawangin.khan@gmail.com>
2019-10-02 20:34:37 -0400

    components deep dive

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from "./Person/Person";
 
-const Persons = (props) => props.persons.map((person, index) => {
-        return <Person
-            name={person.name}
-            age={person.age}
-            click={() => props.clicked(index)}
-            key={person.id}
-            changed={(event) => props.changed(event, person.id)}
-        />
-    });
+
+class Persons extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate');
+        if (
+            nextProps.persons !== this.props.persons || 
+            nextProps.changed !== this.props.changed || 
+            nextProps.clicked !== this.props.clicked 
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate');
+        return null;
+    }
+
+    componentDidUpdate() {
+        console.log('[Persons.js] componentDidUpdate');
+    }
+
+    componentDidMount() {
+        console.log('[Persons.js] componentDidMount');
+    }
+
+    render() {
+        return this.props.persons.map((person, index) => {
+            return (
+                <Person
+                    name={person.name}
+                    age={person.age}
+                    click={() => this.props.clicked(index)}
+                    key={person.id}
+                    changed={(event) => this.props.changed(event, person.id)}
+                />
+            );
+        });   
+    }
+}
 
 export default Persons;
