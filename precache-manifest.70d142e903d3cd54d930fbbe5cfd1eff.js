5106b8159b0e14f3455051ebb03dd65dfe09f6e5
Lawangin Khan <lawangin.khan@gmail.com>
2019-10-21 11:33:16 -0400

    Updates

---
+++ precache-manifest.70d142e903d3cd54d930fbbe5cfd1eff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90cc521b2b2142bbe79f3316203b29b",
+    "url": "/react-app-one/index.html"
+  },
+  {
+    "revision": "942ac65451a0eb362a50",
+    "url": "/react-app-one/static/css/main.e26ed708.chunk.css"
+  },
+  {
+    "revision": "ebbc2ecf1db36f07ed39",
+    "url": "/react-app-one/static/js/2.e15eaa24.chunk.js"
+  },
+  {
+    "revision": "942ac65451a0eb362a50",
+    "url": "/react-app-one/static/js/main.8e0184ff.chunk.js"
+  },
+  {
+    "revision": "99995693526dbe2a0293",
+    "url": "/react-app-one/static/js/runtime-main.71e74e6f.js"
+  }
+]);
